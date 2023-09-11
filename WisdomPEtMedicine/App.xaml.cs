5da4f2f11647f395b260cef761b083ead5301624
Christian.Hernandez <Christian.Hernandez@DLCELPC933>
2023-09-11 15:55:57 -0600

    Agregar archivos de proyecto.

---
+++ WisdomPEtMedicine/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WisdomPEtMedicine
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ WisdomPEtMedicine/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace WisdomPEtMedicine
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
